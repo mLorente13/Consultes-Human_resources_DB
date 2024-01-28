bec5aa18f0faea22204a7ce42e9d82fd73207653
Marc Lorente <mlorente2005@gmail.com>
2024-01-28 19:13:03 +0100

    Exercici6 completat

---
+++ Exercici6.sql
@@ -0,0 +1,8 @@
+SELECT
+    employees.first_name,
+    employees.last_name
+FROM
+    employees
+    INNER JOIN departments ON employees.department_id = departments.department_id
+    INNER JOIN locations ON departments.location_id = locations.location_id
+    WHERE departments.location_id = 2400 || departments.location_id = 2500 || departments.location_id = 2700;
