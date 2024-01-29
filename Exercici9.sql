eb2a46f7dad96a416bdf9eba38cf91ad6442e1af
Marc Lorente <mlorente2005@gmail.com>
2024-01-29 19:00:03 +0100

    Exercici9 completat

---
+++ Exercici9.sql
@@ -0,0 +1,9 @@
+SELECT
+    departments.department_name,
+    AVG(employees.salary) AS average_salary,
+    COUNT(employees.employee_id) AS num_employees
+FROM
+    departments
+    INNER JOIN employees ON departments.department_id = employees.department_id
+GROUP BY
+    departments.department_id, departments.department_name;
